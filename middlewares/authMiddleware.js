c9fbc6966005a88316a7dbbefabb7410bd9f3877
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-11 15:06:08 +0100

    add authMiddleware in authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,17 @@
+import User from './userModel.js';
 
+const authMiddleware = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        req.user = user;
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+};
+
+export default authMiddleware;
