a65ad8bbb75399228c429541472e4e207c275529
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-11 15:25:57 +0100

    add login route in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,10 @@
+import express from 'express';
+import { userLogin } from './userController.js';
+import authMiddleware from './authMiddleware.js';
+import errorMiddleware from './errorMiddleware.js';
 
+const router = express.Router();
+
+router.post('/login', authMiddleware, userLogin, errorMiddleware);
+
+export default router;
