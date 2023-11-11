4a34701b6a00477b945d4cfef2542429c4665564
KerLau <laurynas.kerp@gmail.com>
2023-11-11 18:57:32 +0100

    necessary code deleted

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
@@ -17,10 +15,5 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Server started and running on port : ${port}`);
 });
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
 
 app.use("/api/users", userRoutes);
-// app.use(notFound);
-// app.use(errorHandler);
