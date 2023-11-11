16997cd8c2f947d3d17617929d97dab1df704a8e
KerLau <laurynas.kerp@gmail.com>
2023-11-11 18:50:50 +0100

    problem solved, userController

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connect } from "mongoose";
+
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -13,14 +13,14 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(notFound);
-app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server started and running on port : ${port}`);
+});
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
 app.use("/api/users", userRoutes);
-
-app.listen(port, () => {
-  console.log(`Server started and running on port : ${port}`);
-});
+// app.use(notFound);
+// app.use(errorHandler);
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,11 +4,7 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7 days",
   });
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 30, // 30days
-    sameSite: "strict",
-  });
+
   return token;
 };
 
