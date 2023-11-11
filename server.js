db42253250874b825258b3aef7811a60ec27158c
KerLau <laurynas.kerp@gmail.com>
2023-11-11 15:23:29 +0100

    global error handler added with notFound and errorHandler

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import { connect } from "mongoose";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 6969;
 
 connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started and running on port : ${port}`);
